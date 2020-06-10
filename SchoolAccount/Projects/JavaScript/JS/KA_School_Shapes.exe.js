96968c87a3312b3268b3fdbeaabb16e906b15963
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-09 17:48:48 -0700

    4 files
    4 files (3 JS, 1 INI)

---
+++ SchoolAccount/Projects/JavaScript/JS/KA_School_Shapes.exe.js
@@ -0,0 +1,26 @@
+background(225, 232, 225);
+
+
+rect(76, 45, 250, 300); // face
+fill(0, 200, 255);
+
+rect(126, 250, 152, 60); // mouth
+fill(237, 123, 66);
+
+rect(140, 150, 30, 30); // left eye
+fill(237, 123, 66);
+
+rect(240, 150, 30, 30); // right eye
+fill(237, 123, 66);
+
+rect(175, 345, 50, 50); // neck
+fill(18, 7, 7);
+
+// unibrow
+line(140, 129, 270, 129);
+
+text("orginal character do not steal", 50, 370, 100, 500);
+textFont(100, 100);
+
+    
+
